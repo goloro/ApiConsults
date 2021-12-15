46519c86218415191d62c3f656e6d8a64ea42fc9
goloro <goloro02@gmail.com>
2021-12-15 14:06:44 +0100

    first commit

--- script.js
+++ script.js
@@ -6,24 +6,23 @@ function ajax() {
     const xhr = new XMLHttpRequest()
     let list = document.getElementById("list")
 
-    xhr.open('GET', url)
+    xhr.open('GET', url, true)
     xhr.send()
     xhr.onload = () => {
         if (xhr.status === 200) {
             let data = JSON.parse(xhr.responseText)
-            let list = getElementById("list")
             data.forEach(e => {
                 let ol = document.createElement("ol")
                 for (let i=0; i<4; i++) {
                     let li = document.createElement("li")
 
-                    if (i == 1)
+                    if (i == 0)
                         li.innerHTML = "Name: " + e.name
-                    else if (i == 2)
+                    else if (i == 1)
                         li.innerHTML = "Username: " + e.username
-                    else if (i == 3)
+                    else if (i == 2)
                         li.innerHTML = "Email: " + e.email
-                    else if (i == 4)
+                    else if (i == 3)
                         li.innerHTML = "Phone: " + e.phone
                     
                     ol.appendChild(li)
