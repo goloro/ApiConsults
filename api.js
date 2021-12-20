de6abf3cc48fe221b17d9750132299159477c9d1
goloro <goloro02@gmail.com>
2021-12-20 17:04:30 +0100

    first commit

--- api.js
+++ api.js
@@ -47,40 +47,37 @@ function ajax() {
 }
 
 function fetchFuncion() {
-    fetch ("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
+    // https://www.balldontlie.io/api/v1/teams
+    // https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15
+    fetch ("https://www.balldontlie.io/api/v1/teams")
     .then((res) => {
         return res.json()
     })
     .then(json => {
-        console.log(json)
-        let cont = 0
         let content = document.getElementById("contentApi")
-        json.forEach(e => {
-            if (cont < 11) {
-                let div = document.createElement("div")
-                for (let i=0; i<4; i++) {
-                    let p1 = document.createElement("p")
-                    let p2 = document.createElement("p")
-                    let p3 = document.createElement("p")
-    
-                    if (i == 0)
-                        p1.innerHTML = "Título: " + e.title
-                    else if (i == 1)
-                        p2.innerHTML = "Precio: " + e.normalPrice + "$"
-                    else if (i == 2)
-                        p3.innerHTML = "Id: " + e.gameID
-                        
-                    div.appendChild(p1)
-                    div.appendChild(p2)
-                    div.appendChild(p3)
-                }
-                content.appendChild(div)
-                cont += 1
+        json.data.forEach(e => {
+            let div = document.createElement("div")
+            for (let i=0; i<4; i++) {
+                let p1 = document.createElement("p")
+                let p2 = document.createElement("p")
+                let p3 = document.createElement("p")
+                let p4 = document.createElement("p")
+
+                if (i == 0)
+                    p1.innerHTML = "Name: " + e.full_name
+                else if (i == 1)
+                    p2.innerHTML = "City: " + e.city
+                else if (i == 2)
+                    p3.innerHTML = "Conference: " + e.conference
+                else if (i == 3)
+                    p3.innerHTML = "Division: " + e.division
+                    
+                div.appendChild(p1)
+                div.appendChild(p2)
+                div.appendChild(p3)
+                div.appendChild(p4)
             }
+            content.appendChild(div)
         })
     }).catch().finally()
-}
-
-function axios() {
-    // https://www.balldontlie.io/api/v1/teams
 }
